f0c76993a78ca3ebf092b0e64f2828a3ce074752
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-09-10 22:28:01 -0700

    first commit

--- flask.py
+++ flask.py
@@ -10,3 +10,9 @@ migrate = Migrate(app, db)
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
+@app.cli.command()
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
