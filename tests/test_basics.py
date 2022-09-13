4192c640a69b8abbb0e9f3316e47ca9cb7f5706a
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-09-13 03:57:19 -0700

    First commit

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -3,7 +3,7 @@ from flask import current_app
 from app import create_app, db
 
 class BasicTestCase(unittest.TestCase):
-    def setup(self):
+    def setUp(self):
         self.app = create_app('testing')
         self.app_context = self.app.app_context()
         self.app_context.push()
