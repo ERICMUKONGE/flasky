11f2943ac9e658943cf1073d06337d915feb360e
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-09-13 03:54:46 -0700

    First commit

--- flask.py
+++ flask.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
-@app.shell_context()
+@app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
