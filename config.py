89d8261d0e6de8a59f0ad4d9b02642a772fd836a
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-09-13 02:01:30 -0700

    First commit

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SERCET_KEY = os.environ.get('SERCET_KEY') or 'hard to guess string'
-    MAIL_SERVER = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
         ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
