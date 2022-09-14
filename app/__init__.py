6cc643e32747071babeffa10e8a35205328fc62b
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-09-14 02:20:58 -0700

    First commit

--- app/__init__.py
+++ app/__init__.py
@@ -4,16 +4,21 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
+    login_manager.init_app(app)
 
     bootstrap.init_app(app)
     mail.init_app(app)
