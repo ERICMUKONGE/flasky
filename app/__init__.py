0cd61ceef959884a29cdbee4bc90a2c846890f0c
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-09-13 23:20:06 -0700

    First commit

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,10 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
 
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
