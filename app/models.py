a9babeeae9420bb7c5bdccc174a8b709d3c3628f
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-09-14 02:14:07 -0700

    First commit

--- app/models.py
+++ app/models.py
@@ -1,7 +1,15 @@
+import email
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
-    #...
+    
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key =True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForegnKey('role.id'))
     password_hash = db.Column(db.String(128))
 
     @property
