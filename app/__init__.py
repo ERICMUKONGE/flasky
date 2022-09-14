36a9a68e892e91ab77edf7afea61a3e26cbca085
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-09-14 02:30:43 -0700

    First commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from app.models import User
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
@@ -30,5 +31,9 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))    
 
     return app
