b5272fd9eb31d9789629eac94483479f06ccce9b
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-10-04 14:07:40 -0700

    First commit

--- app/models.py
+++ app/models.py
@@ -1,15 +1,16 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+from . import db
 
 
-class User(db.Model):
-    # __tablename__ = 'users'
-    # id = db.Column(db.Integer, primary_key =True)
-    # email = db.Column(db.String(64), unique=True, index=True)
-    # username = db.Column(db.String(64), unique=True, index=True)
-    # password_hash = db.Column(db.String(128))
-    # role_id = db.Column(db.Integer, db.ForegnKey('role.id'))
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key =True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForegnKey('role.id'))
     password_hash = db.Column(db.String(128))
 
     @property
