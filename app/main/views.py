e386e98e9ad81156e86954df74ce5e60c86536f0
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-10-03 22:18:12 -0700

    First commit

--- app/main/views.py
+++ app/main/views.py
@@ -13,5 +13,5 @@ def index():
         return redirect(url_for('index'))
     return render_template('index.html',
                             form=form, name=session.get('name'),
-                            known=session.get('known',False),
+                            known=session.get('known', False),
                             current_time=datetime.utcnow())    
