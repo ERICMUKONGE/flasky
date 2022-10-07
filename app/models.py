b34e394a076baeceac4b068510c3979173c2a417
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-10-07 05:11:30 -0700

    First commit

--- app/models.py
+++ app/models.py
@@ -4,13 +4,14 @@ from . import login_manager
 from . import db
 
 
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key =True)
+    id = db.Column(db.Integer(), primary_key =True)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
-    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('role.id'))
     password_hash = db.Column(db.String(128))
 
     @property
