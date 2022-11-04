52010c66466cdf579ade8a987f99ddec206fd552
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-11-04 06:18:10 -0700

    First commit

--- flasky.py
+++ flasky.py
@@ -1,83 +1,55 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
 from datetime import datetime
+from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from flask_migrate import Migrate
 import os
 from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail, Message
-from threading import Thread
-from app import create_app, db
-from app.models import Role, User
-
+from flask_migrate import Migrate
+from flask_mail import Mail
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+mail = Mail(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['SQLALCHEMY_DATABASE_URI']=\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db.init_app(app)    
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'stmp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['FLASKY_MAIL_SUBJECT_PREFIX']='[Flasky]'
-app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
-app.config['FLASK_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
-
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
-
-def send_email(to, subject, template, **kwargs):
-    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, 
-                sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
-    msg.body = render_template(template + '.txt', **kwargs) 
-    msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
-    thr.start()
-    return thr 
-    mail.send(msg)
-
-db = SQLAlchemy(app)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
-mail = Mail(app)
-migrate = Migrate(app,db)
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-
-class NameForm(FlaskForm):
-    name= StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    form =NameForm()
+    form = NameForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
             user = User(username=form.name.data)
             db.session.add(user)
-            session['known'] =False
-            if app.config['FLASKY_ADMIN']:
-                send_email(app.config['FLASKY_ADMIN'], 'New User',
-                            'mail/new_user', user=user)
+            db.session.commit()
+            session['known'] = False
         else:
             session['known'] = True
-        session['name'] = form.name.data    
+        session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html',form=form, name=session.get('name'),
-                            known=session.get('known', False))                        
-    
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known',False))            
+
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)  
+    return render_template('user.html', name=name)   
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -85,34 +57,35 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500        
-   
+    return render_template('500'), 500         
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 class Role(db.Model):
-    __tablename__ = 'role'
+    __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')    
+
+class User(db.Model): 
+    __tablename__ = 'users' 
+    id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
 
     def __repr__(self):
-        return '<User %r>' % self.username          
+        return '<User %r>' % self.username
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))            
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
-
-@app.cli.command()
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests) 
+    
+if __name__ == '__main__':
+    app.run(debug=True)    
