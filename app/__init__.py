8c266b84119ef1e57d98e5114511dc5fb4753673
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-11-11 01:32:31 +0300

    First commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
-from flask_moment import SQLAlchemy
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 from config import config
 
 bootstrap = Bootstrap()
