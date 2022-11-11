e6381075b6775777b6638aea507080c23f08b315
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-11-11 04:00:22 +0300

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    
 
     bootstrap.init_app(app)
     mail.init_app(app)
