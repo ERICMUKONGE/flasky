2bc4cd41d7e57f754e16fdb553a938022b6c8d03
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-11-11 03:50:39 +0300

    Update

--- app/models.py
+++ app/models.py
@@ -3,7 +3,6 @@ from flask_login import UserMixin
 from . import login_manager
 from . import db
 
-
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer(), primary_key =True)
