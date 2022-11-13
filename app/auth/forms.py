55ce39c76c3a85327308e08314d61c7dbd5e7150
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-11-14 00:04:01 +0300

    Update

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,7 +3,8 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), 
+                                             Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
