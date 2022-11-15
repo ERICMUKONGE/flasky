7f7efab37cfe893225f4bc1e64480558d04e877c
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-11-15 07:52:23 +0300

    Update

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer(), db.ForeignKey('role.id'))
-    password_hash = db.Column(db.String(128))
+   
 
 class Role(UserMixin, db.Model):
     __tablename__='roles'
