e80d4bf74898779953fb22409e109b80153d814d
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-11-25 01:41:15 +0300

    Update

--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,14 @@
+
 from datetime import datetime
 from flask import render_template, session, redirect, url_for
 from . import main
-from .forms import NameForm
+from flask_wtf import FlaskForm
 from .. import db
 from ..models import User
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        ...
-        return redirect(url_for('.index'))
     return render_template('index.html',
-                           form=form, name=session.get('name'),
+                           form=FlaskForm, name=session.get('name'),
                           known=session.get('known', False),
                            current_time=datetime.utcnow())    
