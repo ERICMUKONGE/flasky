7eeffc2b92b1703289274282e80da9f059534dd3
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-11-27 08:09:17 +0300

    Update

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager, db
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import URLSafeTimedSerializer as Serializer
 from flask import current_app
 
 class User(UserMixin, db.Model):
@@ -9,8 +9,8 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key =True)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer(), db.ForeignKey('role.id'))
     password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer(), db.ForeignKey('role.id'))
     confirmed = db.Column(db.Boolean, default=False)
 
     def generate_confirmation_token(self, expiration=3600):
@@ -29,41 +29,41 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True          
 
-class Role(UserMixin, db.Model):
-    __tablename__='roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    default = db.Column(db.Boolean,default=False, index=True)
-    permissions = db.Column(db.Integer)
-    users = db.relationship('User', backref='role', lazy='dynamic')
+# class Role(UserMixin, db.Model):
+#     __tablename__='roles'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(64), unique=True)
+#     default = db.Column(db.Boolean,default=False, index=True)
+#     permissions = db.Column(db.Integer)
+#     users = db.relationship('User', backref='role', lazy='dynamic')
 
-    def __init__(self, **kwargs):
-        super(Role, self).__init__(**kwargs)
-        if self.permissions is None:
-            self.permissions = 0
+#     def __init__(self, **kwargs):
+#         super(Role, self).__init__(**kwargs)
+#         if self.permissions is None:
+#             self.permissions = 0
 
 
-    @staticmethod
-    def insert_roles():
-        roles = {
-            'User':[Permission.FOLLOW, Permission.COMMENT, Permission.WRITE],
-            'Moderator':[Permission.FOLLOW, Permission.COMMENT,
-                         Permission.WRITE, Permission.MODERATE],
-            'Administrator':[Permission.FOLLOW, Permission.COMMENT,
-                         Permission.WRITE, Permission.MODERATE,
-                         Permission.ADMIN],             
-        }
-        default_role = 'User'
-        for r in roles:
-            role = Role.query.filter_by(name=r).first()
-            if role is None:
-                role = Role(name=r)
-            role.reset_permissions()
-            for perm in roles[r]:
-                role.add_permission(perm)
-            role.default = (role.name == default_role)
-            db.session.add(role)
-        db.session.commit()
+#     @staticmethod
+#     def insert_roles():
+#         roles = {
+#             'User':[Permission.FOLLOW, Permission.COMMENT, Permission.WRITE],
+#             'Moderator':[Permission.FOLLOW, Permission.COMMENT,
+#                          Permission.WRITE, Permission.MODERATE],
+#             'Administrator':[Permission.FOLLOW, Permission.COMMENT,
+#                          Permission.WRITE, Permission.MODERATE,
+#                          Permission.ADMIN],             
+#         }
+#         default_role = 'User'
+#         for r in roles:
+#             role = Role.query.filter_by(name=r).first()
+#             if role is None:
+#                 role = Role(name=r)
+#             role.reset_permissions()
+#             for perm in roles[r]:
+#                 role.add_permission(perm)
+#             role.default = (role.name == default_role)
+#             db.session.add(role)
+#         db.session.commit()
                         
     @property
     def password(self):
@@ -79,7 +79,4 @@ class Role(UserMixin, db.Model):
     @login_manager.user_loader
     def load_user(user_id): 
         return User.query.get(int(user_id)) 
-        @app.route('/secret')
-        @login_required
-        def secret():
-            return'Only authenticate users are allowed!'             
+                    
