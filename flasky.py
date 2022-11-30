006c140faba7b9de8195edd7d69a9bd5f2055a6f
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-11-30 06:57:50 +0300

    update

--- flasky.py
+++ flasky.py
@@ -7,7 +7,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import os
 from app import create_app, db
-from app.models import User, Role
+from app.models import User
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
