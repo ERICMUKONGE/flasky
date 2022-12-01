6384f6cff2539f7fa4de312bfe936056052f3cdd
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-12-01 16:44:58 +0300

    Update

--- app/auth/views.py
+++ app/auth/views.py
@@ -43,6 +43,7 @@ def logout():
 # def register():
 #     form = RegistrationForm()
 #     if form.validate_on_submit():
+            #...
 #         user = User(email=form.email.data,
 #                    username=form.username.data,
 #                    password=form.password.data)
