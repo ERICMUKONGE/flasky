ef163d802fe39c41437dc5a8a942a92a02d92131
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-12-15 11:57:36 +0300

    First commit

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -19,4 +19,20 @@ class UserModelTestCase(unittest.TestCase):
     def test_password_salts_are_random(self):
         u = User(password='cat')
         u2 = User(password='cat')
-        self.assertTrue(u.password_hash != u2.password_hash)            
+        self.assertTrue(u.password_hash != u2.password_hash)  
+
+    def test_user_role(self):
+        u = User(email='john@example.com', password='cat')
+        self.assertTrue(u.can(Permission.FOLLOW))
+        self.assertTrue(u.can(Permission.COMMENT)) 
+        self.assertTrue(u.can(Permission.WRITE)) 
+        self.assertFalse(u.can(Permission.MODERATE)) 
+        self.assertFalse(u.can(Permission.ADMIN))   
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission.FOLLOW))
+        self.assertFalse(u.can(Permission.COMMENT)) 
+        self.aassertFalse(u.can(Permission.WRITE)) 
+        self.assertFalse(u.can(Permission.MODERATE)) 
+        self.assertFalse(u.can(Permission.ADMIN))               
