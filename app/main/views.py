da3a0c14687b413f33038f03a085b9913cc97c79
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-12-19 22:35:48 +0300

    Update

--- app/main/views.py
+++ app/main/views.py
@@ -4,13 +4,13 @@ from flask import render_template, session, redirect, url_for
 from . import main
 from ..models import User
 
-@main.route('/', methods=['GET', 'POST'])
-def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        # ...
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=session.get('name'),
-                           known=session.get('known', False),
-                           current_time=datetime.utcnow())    
+# @main.route('/', methods=['GET', 'POST'])
+# def index():
+#     form = NameForm()
+#     if form.validate_on_submit():
+#         # ...
+#         return redirect(url_for('.index'))
+#     return render_template('index.html',
+#                            form=form, name=session.get('name'),
+#                            known=session.get('known', False),
+#                            current_time=datetime.utcnow())    
